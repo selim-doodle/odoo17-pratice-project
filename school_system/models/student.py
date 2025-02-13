8b958034486176b821808ebac0fb83d767d052f7
Selim-reza <selim@thedoodleinc.com>
2025-02-13 13:11:33 +0600

    new field added in student module, added search and graph view

--- school_system/models/student.py
+++ school_system/models/student.py
@@ -1,4 +1,5 @@
 from odoo import api, models, fields
+from .constants import GENDER_SELECTION
 
 class Student(models.Model):
     _name = 'school.student'
@@ -6,4 +7,6 @@ class Student(models.Model):
 
     name = fields.Char(string="Student name")
     dob = fields.Date(string="Date of Birth")
+    gender = fields.Selection(GENDER_SELECTION, string="Select gender")
+    admission_date = fields.Date(string="Admission date")
     className = fields.Many2one('school.classroom', string="Class")
