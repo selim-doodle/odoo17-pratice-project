1fe5d4f9b7b8718ba9babc994108d85bcc6fbe80
Selim-reza <selim@thedoodleinc.com>
2025-02-14 15:57:40 +0600

    deleted Project_manager and created new project_tracker module to understand one2many more clearly

---
+++ projects_tracker/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'projects tracking system',
+    'website': 'www.facebook.com',
+    'author': 'Selim Reza',
+    'data': [
+        'views/menu.xml',
+        'views/project.xml',
+        'views/task.xml',
+        'security/ir.model.access.csv',
+    ],
+    'depends': [],
+    'application': True,
+    'sequence': -3,
+}
--- projects_tracker/models/project.py
+++ projects_tracker/models/project.py
@@ -6,7 +6,7 @@ class Project(models.Model):
     _description = 'Project'
 
     name = fields.Char(string='Project Name', required=True)
-    task_ids = fields.One2many('project.task', 'project_id', string='Tasks')
+    signed_at = fields.Date(string="Signed at")
 
 
 class Task(models.Model):
@@ -14,4 +14,4 @@ class Task(models.Model):
     _description = 'Task'
 
     name = fields.Char(string='Task Name', required=True)
-    project_id = fields.Many2one('project.project', string='Project', required=True)
+    time_required = fields.Integer(string="Required time(Hour)")
