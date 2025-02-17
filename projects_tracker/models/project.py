4eed959cb075a02526ac6b572ce308b97c8454cd
Selim-reza <selim@thedoodleinc.com>
2025-02-17 17:08:28 +0600

    hiding column based on specific value is done in project module

--- projects_tracker/models/project.py
+++ projects_tracker/models/project.py
@@ -1,3 +1,5 @@
+from email.policy import default
+
 from odoo import models, fields
 
 
@@ -9,7 +11,7 @@ class Project(models.Model):
     project_title = fields.Char(string='Project Name', required=True)
     signed_at = fields.Date(string="Signed at")
     project_line_ids = fields.One2many('project.project.line', 'project_id', string="Task lines")
-
+    hide_required_time = fields.Boolean(default=False, string="Hide required time")
 
 class ProjectProjectLine(models.Model):
     _name = "project.project.line"
@@ -18,5 +20,5 @@ class ProjectProjectLine(models.Model):
 
     task_id = fields.Many2one('project.task')
     assigned_date = fields.Date(string="Start date")
-    project_id = fields.Many2one('project.project', string="Projects")
     required_time = fields.Integer(related="task_id.time_required", string="Required time")
+    project_id = fields.Many2one('project.project', string="Projects")
