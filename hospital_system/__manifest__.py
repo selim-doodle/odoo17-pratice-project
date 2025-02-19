c80b5104bdfe07188ce439869d9e533d815fb8a9
Selim-reza <selim@thedoodleinc.com>
2025-02-19 18:35:39 +0600

    added machanism of adding data from xml and csv file

--- hospital_system/__manifest__.py
+++ hospital_system/__manifest__.py
@@ -3,6 +3,8 @@
     'sequence': -3,
     'data': [
         'security/ir.model.access.csv',
+        'data/hospital_patient_data.xml',
+        'data/hospital.patient.csv',
         'wizard/cancel_appointment.xml',
         'views/menu.xml',
         'views/patient.xml',
